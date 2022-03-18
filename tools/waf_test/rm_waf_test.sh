0c9cfe6cdfa53c852e9a19fcea8c7819f89313ce
gfeng <gfeng@neuvector.com>
2022-03-18 16:13:28 -0700

    WAF test environment set up and send attack traffic

---
+++ tools/waf_test/rm_waf_test.sh
@@ -0,0 +1,6 @@
+kubectl delete -f waf_group_httpd.yml
+kubectl delete -f waf_traversal.yml
+kubectl delete -f waf_injection.yml
+kubectl delete -f waf_cross.yml
+kubectl delete -f waf_execution.yml
+kubectl delete -f http_backend.yaml
