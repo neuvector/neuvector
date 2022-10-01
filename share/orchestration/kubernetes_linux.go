7c3a01f89d6f408fa1be4aaff6c14fff196706c1
gfeng <gfeng@neuvector.com>
2022-10-01 11:37:00 -0700

    NVSHAS-6703, with Antrea CNI, traffic between pods within the same node see source as unmanaged workload

--- share/orchestration/kubernetes_linux.go
+++ share/orchestration/kubernetes_linux.go
@@ -10,6 +10,10 @@ func (d *kubernetes) isTunnelInterface(name, kind string) bool {
 	if strings.HasPrefix(name, "tun") && (kind == "ipip" || kind == "openvswitch") {
 		return true
 	}
+	//antreaCNI's antrea-gw0 i/f
+	if strings.HasPrefix(name, "antrea") &&  kind == "openvswitch" {
+		return true
+	}
 	// flannel.1
 	if strings.HasPrefix(name, "flannel") && kind == "vxlan" {
 		return true
