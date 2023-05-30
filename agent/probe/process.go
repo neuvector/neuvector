f8ab19e4b81aec9cffa5dcfa7281f101ec894d1e
Jaime Yu <jyu@Jaime-Suses-MacBook-Pro.local>
2023-05-30 12:13:10 -0600

    Changed trace to debug

--- agent/probe/process.go
+++ agent/probe/process.go
@@ -563,7 +563,7 @@ func (p *Probe) removeProcessInContainer(pid int, id string) {
 				} else {
 					// no valid rootPid: it could be a "flash" container, delete it
 					// for example: "docker run ubuntu echo 123"
-					mLog.WithFields(log.Fields{"pid": pid}).Trace("PROC: orphan container")
+					mLog.WithFields(log.Fields{"pid": pid}).Debug("PROC: orphan container")
 				}
 			}
 		}
