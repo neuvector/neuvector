de91f796f8a1b5977fceb21ea1b6b6c702b9b7b1
becitsthere <becitsthere@gmail.com>
2023-08-01 20:33:27 -0700

    NVSHAS-8172: Not to write local file during benchmark

--- agent/nvbench/docker-bench-security/tests/6_docker_security_operations.sh
+++ agent/nvbench/docker-bench-security/tests/6_docker_security_operations.sh
@@ -17,7 +17,7 @@ check_6_1() {
   local check="$id - $desc"
   starttestjson "$id" "$desc"
 
-  images=$(docker images -q | sort -u | wc -l | awk '{print $1}')
+  images=$(docker images -q | sort -u | wc -l | cut -d " " -f 1)
   active_images=0
 
   for c in $(docker inspect --format "{{.Image}}" "$(docker ps -qa)" 2>/dev/null); do
@@ -43,8 +43,8 @@ check_6_2() {
   local check="$id - $desc"
   starttestjson "$id" "$desc"
 
-  total_containers=$(docker info 2>/dev/null | grep "Containers" | awk '{print $2}')
-  running_containers=$(docker ps -q | wc -l | awk '{print $1}')
+  total_containers=$(docker info 2>/dev/null | grep "Containers" | cut -d " " -f 2)
+  running_containers=$(docker ps -q | wc -l | cut -d " " -f 1)
   diff="$((total_containers - running_containers))"
   info -c "$check"
   if [ "$diff" -gt 25 ]; then
