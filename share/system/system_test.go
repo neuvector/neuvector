076c04671c9591a345037c29e4e0a7143c0dd997
Jaime Yu <jaime.yu@suse.com>
2023-08-21 11:55:13 -0600

    Removed TestCgroupVersion() unit test - feedback from jay

--- share/system/system_test.go
+++ share/system/system_test.go
@@ -1,7 +1,6 @@
 package system
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -43,32 +42,4 @@ func TestCgroupSelector(t *testing.T) {
 	assert.Equal(t,
 		"/host/cgroup/memory",
 		sys.cgroupMemoryDir, "For unsupported, we should default to v1")
-}
-
-func TestCgroupVersion(t *testing.T) {
-
-	var sys SystemTools
-	// When we upgrade golang, use TempDir
-	//sys.cgroupDir = T.TempDir()
-	sys.cgroupDir = "/tmp"
-	tmpcontrollerpath := "/tmp/cgroup.controllers"
-	// Make sure there isn't one here
-	os.Remove(tmpcontrollerpath) // Best effort
-
-	// Test v1
-	version := sys.DetermineCgroupVersion()
-	assert.Equal(t, cgroup_v1, version, "Should be cgroup v1")
-
-	// Test V2
-	cgroupcontroller := tmpcontrollerpath
-	fp, err := os.Create(cgroupcontroller)
-	if err != nil {
-		t.Fatalf("Could not create tmp file for testing: %s", err.Error())
-	}
-	fp.Close() // We don't need
-
-	version2 := sys.DetermineCgroupVersion()
-	assert.Equal(t, cgroup_v2, version2, "Should be cgroup v1")
-
-	os.Remove(tmpcontrollerpath) // best effort
 }
