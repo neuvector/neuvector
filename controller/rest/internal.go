177d91d6fad068075b9b634a211e16bbb1096875
wlin <wlin@neuvector.com>
2023-11-14 16:34:53 -0800

    NVSHAS-8162: To persist the accept status for RBAC error notifications

--- controller/rest/internal.go
+++ controller/rest/internal.go
@@ -1,12 +1,20 @@
 package rest
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/neuvector/neuvector/controller/access"
+	"github.com/neuvector/neuvector/controller/api"
+	"github.com/neuvector/neuvector/controller/common"
 	"github.com/neuvector/neuvector/share"
+	"github.com/neuvector/neuvector/share/cluster"
+	"github.com/neuvector/neuvector/share/utils"
 )
 
 func handlerInternalSystem(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -23,4 +31,113 @@ func handlerInternalSystem(w http.ResponseWriter, r *http.Request, ps httprouter
 	resp := cacher.GetRiskScoreMetrics(accSysConfig, acc)
 
 	restRespSuccess(w, r, resp, acc, login, nil, "Get internal system data")
+}
+
+func handlerAcceptAlert(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	log.WithFields(log.Fields{"URL": r.URL.String()}).Debug("")
+	defer r.Body.Close()
+
+	acc, login := getAccessControl(w, r, "")
+	if acc == nil {
+		return
+	} else if login.fullname == "" || login.loginType == loginTypeApikey {
+		restRespAccessDenied(w, login)
+		return
+	}
+
+	var rconf api.RESTAcceptedAlerts
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &rconf)
+	if err != nil {
+		log.WithFields(log.Fields{"error": err}).Error("Request error")
+		restRespError(w, http.StatusBadRequest, api.RESTErrInvalidRequest)
+		return
+	}
+
+	supportedUserAlerts := utils.NewSet(share.AlertPwdExpiring, share.AlertAdminHasDefPwd)
+	supportedManagerAlerts := utils.NewSet(share.AlertNvNewVerAvailable, share.AlertNvInMultiVersions, share.AlertCveDbTooOld)
+	confAccepted := map[string][]string{
+		"manager":    rconf.ManagerAlerts,
+		"controller": rconf.ControllerAlerts,
+		"user":       rconf.UserAlerts,
+	}
+	for alertSrc, newAccepted := range confAccepted {
+		if len(newAccepted) == 0 {
+			continue
+		}
+
+		var userName string
+		if alertSrc != "user" {
+			if !acc.CanWriteCluster() && !acc.IsFedAdmin() {
+				continue
+			}
+			userName = common.ReservedNvSystemUser
+		} else {
+			if login.mainSessionUser != "" {
+				// for login session from master cluster, do not allow accepting user alerts
+				continue
+			}
+			userName = login.fullname
+		}
+
+		retry := 0
+		for retry < retryClusterMax {
+			user, rev, _ := clusHelper.GetUserRev(userName, access.NewReaderAccessControl())
+			if user == nil && userName == common.ReservedNvSystemUser {
+				secret, _ := utils.GetGuid()
+				u := share.CLUSUser{
+					Fullname:     common.ReservedNvSystemUser,
+					Username:     common.ReservedNvSystemUser,
+					PasswordHash: utils.HashPassword(secret),
+					Domain:       "",
+					Role:         api.UserRoleNone,
+					Timeout:      common.DefIdleTimeoutInternal,
+					RoleDomains:  make(map[string][]string),
+					Locale:       common.OEMDefaultUserLocale,
+					PwdResetTime: time.Now().UTC(),
+				}
+				value, _ := json.Marshal(u)
+				key := share.CLUSUserKey(common.ReservedNvSystemUser)
+				cluster.PutIfNotExist(key, value, false)
+				user, rev, _ = clusHelper.GetUserRev(common.ReservedNvSystemUser, acc)
+			}
+			if user == nil {
+				restRespAccessDenied(w, login)
+				return
+			}
+
+			acceptedAlerts := utils.NewSetFromStringSlice(user.AcceptedAlerts)
+			for _, alert := range newAccepted {
+				switch alertSrc {
+				case "manager":
+					if !supportedManagerAlerts.Contains(alert) {
+						continue
+					}
+				case "controller":
+					if len(alert) != 32 {
+						continue
+					}
+				case "user":
+					if !supportedUserAlerts.Contains(alert) {
+						continue
+					}
+				}
+				acceptedAlerts.Add(alert)
+			}
+			user.AcceptedAlerts = acceptedAlerts.ToStringSlice()
+			if err = clusHelper.PutUserRev(user, rev); err != nil {
+				log.WithFields(log.Fields{"error": err, "rev": rev}).Error()
+				retry++
+			} else {
+				break
+			}
+		}
+		if retry >= retryClusterMax {
+			e := "Failed to write to the cluster"
+			restRespErrorMessage(w, http.StatusInternalServerError, api.RESTErrFailWriteCluster, e)
+			return
+		}
+	}
+
+	restRespSuccess(w, r, nil, acc, login, nil, "Accept alerts")
 }
