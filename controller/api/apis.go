491b7b476915da8b102f14ddfe8060906dc4337d
gfeng <gfeng@neuvector.com>
2024-02-01 14:36:27 -0800

    NVSHAS-8423, detect group level bandwidth, active session count and session-rate violation based on configured per group threshold

--- controller/api/apis.go
+++ controller/api/apis.go
@@ -134,6 +134,8 @@ const DlpRulePatternMaxNum int = 16
 const DlpRulePatternMaxLen int = 512
 const DlpRulePatternTotalMaxLen int = 1024
 
+const GrpMetricMax uint32 = (1<<32-1)
+
 const ConfSectionAll string = "all"
 const ConfSectionUser string = "user"
 const ConfSectionPolicy string = "policy"
@@ -1191,6 +1193,10 @@ type RESTGroupBrief struct {
 	PlatformRole    string   `json:"platform_role"`
 	CfgType         string   `json:"cfg_type"` // CfgTypeLearned / CfgTypeUserCreated / CfgTypeGround / CfgTypeFederal (see above)
 	BaselineProfile string   `json:"baseline_profile"`
+	MonMetric       bool     `json:"mon_metric"`
+	GrpSessCur      uint32   `json:"grp_sess_cur"`
+	GrpSessRate     uint32   `json:"grp_sess_rate"`
+	GrpBandWidth    uint32   `json:"grp_band_width"`
 	RESTGroupCaps
 }
 
@@ -1215,6 +1221,10 @@ type RESTGroupConfig struct {
 	Comment  *string              `json:"comment"`
 	Criteria *[]RESTCriteriaEntry `json:"criteria,omitempty"`
 	CfgType  string               `json:"cfg_type"` // CfgTypeLearned / CfgTypeUserCreated / CfgTypeGround / CfgTypeFederal (see above)
+	MonMetric    *bool            `json:"mon_metric,omitempty"`
+	GrpSessCur   *uint32          `json:"grp_sess_cur,omitempty"`
+	GrpSessRate  *uint32          `json:"grp_sess_rate,omitempty"`
+	GrpBandWidth *uint32          `json:"grp_band_width,omitempty"`
 }
 
 type RESTCrdGroupConfig struct {
