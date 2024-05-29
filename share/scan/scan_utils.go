48f10a3bc9d6142b131beb7c0cd53cec82be9f8d
jwhuang <jwhuang@neuvector.com>
2024-05-29 15:02:20 -0700

    NVSHAS-9071: some modules are not reported in the container scan only

--- share/scan/scan_utils.go
+++ share/scan/scan_utils.go
@@ -227,7 +227,7 @@ func (s *ScanUtil) GetAppPackages(path string) ([]AppPackage, []byte, share.Scan
 
 func (s *ScanUtil) getContainerAppPkg(pid int) ([]byte, error) {
 	apps := NewScanApps(false) // no need to scan the same file twice
-	exclDirs := utils.NewSet("bin", "boot", "dev", "proc", "run", "sys", "tmp")
+	exclDirs := utils.NewSet("boot", "dev", "proc", "run", "sys")
 	rootPath := s.sys.ContainerFilePath(pid, "/")
 	rootLen := len(rootPath)
 
