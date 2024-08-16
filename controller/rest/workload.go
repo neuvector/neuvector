d9948f670e84d85d7028ff164b2e312aac39b360
Jeff Huang <jeffhuang4704@gmail.com>
2024-08-16 12:55:48 -0700

    resolve review comments

--- controller/rest/workload.go
+++ controller/rest/workload.go
@@ -11,6 +11,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/neuvector/neuvector/controller/access"
 	"github.com/neuvector/neuvector/controller/api"
 	"github.com/neuvector/neuvector/controller/rpc"
 	"github.com/neuvector/neuvector/share"
@@ -235,7 +236,14 @@ func handlerWorkloadListBase(apiVer string, w http.ResponseWriter, r *http.Reque
 	log.WithFields(log.Fields{"URL": r.URL.String()}).Debug("")
 	defer r.Body.Close()
 
-	acc, login := getAccessControl(w, r, "")
+	var accessOp access.AccessOP
+	if r.Method == http.MethodPost {
+		accessOp = access.AccessOPRead
+	} else {
+		accessOp = ""
+	}
+
+	acc, login := getAccessControl(w, r, accessOp)
 	if acc == nil {
 		return
 	}
@@ -269,9 +277,9 @@ func handlerWorkloadListBase(apiVer string, w http.ResponseWriter, r *http.Reque
 		body, _ := io.ReadAll(r.Body)
 		err := json.Unmarshal(body, &idListData)
 		if err != nil {
-			// TODO: error handling
 			log.WithFields(log.Fields{"error": err}).Error("Request error")
 			restRespError(w, http.StatusBadRequest, api.RESTErrInvalidRequest)
+			return
 		}
 		idlist = utils.NewSetFromStringSlice(idListData.IDs)
 	}
