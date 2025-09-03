611c393b3493c28b9395988038d69ea4fd137ac9
william.lin <william.lin@suse.com>
2025-09-03 08:44:16 -0700

    NVSHAS-10066: First time login does not show link for bootstrap helper when user directly access UI in Rancher deployment

--- controller/rest/eula.go
+++ controller/rest/eula.go
@@ -18,24 +18,27 @@ import (
 )
 
 func handlerEULAShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.WithFields(log.Fields{"URL": r.URL.String()}).Debug("")
+	log.WithFields(log.Fields{"URL": r.URL.String()}).Debug()
 	defer r.Body.Close()
 
+	var eula share.CLUSEULA
 	var resp api.RESTEULAData
 
+	var rssoHeader string
+	if localDev.Host.Platform == share.PlatformKubernetes && localDev.Host.Flavor == share.FlavorRancher {
+		rssoHeader = r.Header.Get(api.RESTRancherSSOHeader)
+	}
+
+	resp.EULA = &api.RESTEULA{}
 	key := share.CLUSConfigEULAKey
 	value, _ := cluster.Get(key)
 	if value != nil {
-		var eula share.CLUSEULA
 		if err := json.Unmarshal(value, &eula); err != nil {
 			log.WithFields(log.Fields{"error": err}).Error("Unmarshal")
 		}
-
-		resp.EULA = &api.RESTEULA{Accepted: eula.Accepted}
-	} else {
-		resp.EULA = &api.RESTEULA{Accepted: false}
+		resp.EULA.Accepted = eula.AcceptedBySSO || eula.Accepted
 	}
-	if k8sPlatform && !resp.EULA.Accepted {
+	if k8sPlatform && rssoHeader != "true" && !eula.Accepted {
 		var errs []string
 		k8sRequiredRoles := []string{resource.NvRbacRole, resource.NvSecretRole, resource.NvSecretControllerRole}
 		if errs, _ = resource.VerifyNvRbacRoles(k8sRequiredRoles, false, true); len(errs) == 0 {
@@ -44,9 +47,9 @@ func handlerEULAShow(w http.ResponseWriter, r *http.Request, ps httprouter.Param
 		if len(errs) > 0 {
 			resp.K8sRbacAlertMsg = strings.Join(errs, "<p>")
 		}
-		if bootstrapPwd, _ := resource.RetrieveBootstrapPassword(); bootstrapPwd != "" {
-			user, _, _ := clusHelper.GetUserRev(common.DefaultAdminUser, access.NewReaderAccessControl())
-			if user != nil && user.ResetPwdInNextLogin && user.UseBootstrapPwd {
+		user, _, _ := clusHelper.GetUserRev(common.DefaultAdminUser, access.NewReaderAccessControl())
+		if user != nil && user.ResetPwdInNextLogin && user.UseBootstrapPwd {
+			if bootstrapPwd, _ := resource.RetrieveBootstrapPassword(); bootstrapPwd != "" {
 				strK8sCmdFormat := `kubectl get secret --namespace %s neuvector-bootstrap-secret -o go-template='{{ .data.bootstrapPassword|base64decode}}{{ "\n" }}'`
 				resp.BootstrapPwdCmd = fmt.Sprintf(strK8sCmdFormat, resource.NvAdmSvcNamespace)
 			}
@@ -56,7 +59,7 @@ func handlerEULAShow(w http.ResponseWriter, r *http.Request, ps httprouter.Param
 }
 
 func handlerEULAConfig(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.WithFields(log.Fields{"URL": r.URL.String()}).Debug("")
+	log.WithFields(log.Fields{"URL": r.URL.String()}).Debug()
 	defer r.Body.Close()
 
 	acc, login := getAccessControl(w, r, "")
@@ -75,7 +78,11 @@ func handlerEULAConfig(w http.ResponseWriter, r *http.Request, ps httprouter.Par
 	}
 
 	var cconf share.CLUSEULA
-	cconf.Accepted = rconf.EULA.Accepted
+	if login != nil && strings.HasPrefix(login.mainSessionID, _rancherSessionPrefix) {
+		cconf.AcceptedBySSO = rconf.EULA.Accepted
+	} else {
+		cconf.Accepted = rconf.EULA.Accepted
+	}
 
 	key := share.CLUSConfigEULAKey
 	value, _ := json.Marshal(&cconf)
