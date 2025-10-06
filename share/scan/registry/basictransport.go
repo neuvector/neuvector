82830f074d7a15558ef2ea83f0e5e17188887c11
william.lin <william.lin@suse.com>
2025-10-06 12:46:25 -0700

    NVSHAS-9761: Harbor registry with s3 storage backend repository scan error

--- share/scan/registry/basictransport.go
+++ share/scan/registry/basictransport.go
@@ -15,21 +15,39 @@ type BasicTransport struct {
 
 func (t *BasicTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	// in the case of jfrog http redirect calls, basic auth is needed
-	oPublic := strings.Contains(t.URL, "redhat.com") || strings.Contains(t.URL, "amazonaws.com") || strings.Contains(t.URL, "azurecr.io") ||
-		strings.Contains(t.URL, "docker.io") || strings.Contains(t.URL, "docker.com") ||
-		strings.Contains(t.URL, "icr.io") || strings.Contains(t.URL, "gitlab.com")
-	nPublic := strings.Contains(req.URL.String(), "redhat.com") || strings.Contains(req.URL.String(), "amazonaws.com") || strings.Contains(req.URL.String(), "azurecr.io") ||
-		strings.Contains(req.URL.String(), "docker.io") || strings.Contains(req.URL.String(), "docker.com") ||
-		strings.Contains(req.URL.String(), "icr.io") || strings.Contains(req.URL.String(), "gitlab.com")
+	oPublic := false
+	nPublic := false
+	publicDomains := []string{"redhat.com", "amazonaws.com", "azurecr.io", "docker.io", "docker.com", "icr.io", "gitlab.com"}
+	for _, domain := range publicDomains {
+		if strings.Contains(t.URL, domain) {
+			oPublic = true
+			break
+		}
+	}
+	for _, domain := range publicDomains {
+		if strings.Contains(req.URL.String(), domain) {
+			nPublic = true
+			break
+		}
+	}
 
 	// log.WithFields(log.Fields{"req": req.URL.String(), "url": t.URL, "oPublic": oPublic, "nPublic": nPublic}).Debug()
 
+	basicAuthSet := false
 	// add auth header if redirect URL is same as the original; if not, re-auth unless neither original or redirecting URL is public
 	if strings.HasPrefix(req.URL.String(), t.URL) || (!oPublic && !nPublic) {
 		if t.Username != "" || t.Password != "" {
 			req.SetBasicAuth(t.Username, t.Password)
+			basicAuthSet = true
 		}
 	}
 	resp, err := t.Transport.RoundTrip(req)
+	if resp != nil {
+		if resp.StatusCode == http.StatusBadRequest && basicAuthSet {
+			req.Header.Del("Authorization")
+			resp, err = t.Transport.RoundTrip(req)
+		}
+	}
+
 	return resp, err
 }
