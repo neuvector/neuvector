45827ffb322b4dced6652a02c3e769af8be22cad
kyledong <kyledong@Ubuntu20.04.myguest.virtualbox.org>
2026-09-09 17:16:21 -0400

    fix: prevent command injection in sniffer filter

--- tools/nstools/nstools.h
+++ tools/nstools/nstools.h
@@ -7,3 +7,7 @@
 
 #define NS_COUNT 6
 
+int nsget(const char *mntns, const char *filepath, int bin, int start, int len);
+int nsrun(const char *mntns, const char **nss, const char *script, int bin, int from_stdin);
+int nsexec(const char *mntns, const char **nss, char *const *cmd);
+int nsexist(const char *mntns, const char *file);
