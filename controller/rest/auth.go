c7cddaef2bcef5bd6b27e1a6e8e597e71c8212fd
neuvector <william.lin@suse.com>
2026-09-10 23:40:13 +0000

    fix: logout bypass via alternate JWT spelling

--- controller/rest/auth.go
+++ controller/rest/auth.go
@@ -1394,7 +1394,7 @@ func jwtValidateToken(encryptedToken, secret string, rsaPublicKey *rsa.PublicKey
 			return nil, fmt.Errorf("jwt certificate expired: %v", jwtCert.jwtPublicKeyNotAfter)
 		}
 		return publicKey, nil
-	})
+	}, jwt.WithStrictDecoding())
 
 	// Try with old cert if it's available.
 	// Note: Ideally we should use extra info stored in claims to do a lookup below.
@@ -1408,7 +1408,7 @@ func jwtValidateToken(encryptedToken, secret string, rsaPublicKey *rsa.PublicKey
 				return nil, fmt.Errorf("jwt certificate expired: %v", jwtCert.jwtOldPublicKeyNotAfter)
 			}
 			return alternativeKey, nil
-		})
+		}, jwt.WithStrictDecoding())
 	}
 
 	if err != nil {
