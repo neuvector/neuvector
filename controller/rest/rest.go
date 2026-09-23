e62e174b0317deb0aca35f19fee4219c8a876c4c
William Lin <7476438+williamlin-suse@users.noreply.github.com>
2026-09-23 10:20:20 -0700

    Revert "automated federation join failed with controller ha setup (#2866) (#2…"
    This reverts commit a0d93e9b08c94193dc706b71b0e41c817b00e4a5.

--- controller/rest/rest.go
+++ controller/rest/rest.go
@@ -2039,8 +2039,6 @@ func startFedRestServer(fedPingInterval uint32) {
 		return
 	} else {
 		_masterClusterIP = m.MasterCluster.RestInfo.Server
-		_fixedJoinToken = m.MasterCluster.FixedJoinToken
-		_allowSameK8sUidRejoin = m.MasterCluster.AllowSameK8sUidRejoin
 	}
 
 	fedRestServerMutex.Lock()
